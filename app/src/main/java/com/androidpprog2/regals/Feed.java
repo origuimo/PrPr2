05d3ac9692c97bd5f986a35436894dfe3f7e6c8e
Paula Fernández Lago <paula.fernandez@students.salle.url.edu>
2023-05-06 17:28:38 +0200

    menu

---
+++ app/src/main/java/com/androidpprog2/regals/Feed.java
@@ -0,0 +1,38 @@
+package com.androidpprog2.regals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        ImageButton regal = findViewById(R.id.regal_img);
+        regal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), frame_layout.class); // frame_layout == llista regals ??
+                startActivity(intent);
+            }
+        });
+
+        ImageButton persones = findViewById(R.id.persones);
+        persones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditProfile.class); //S'ha de canviar a la pantalla d'amics quan l'haguem creat
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
