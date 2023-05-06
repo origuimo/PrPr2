6b8c37e64e5b08ffdafe2666574de9d8243566ba
Paula Fernández Lago <paula.fernandez@students.salle.url.edu>
2023-05-06 17:16:56 +0200

    perfil

--- app/src/main/java/com/androidpprog2/regals/EditProfile.java
+++ app/src/main/java/com/androidpprog2/regals/EditProfile.java
@@ -2,7 +2,10 @@ package com.androidpprog2.regals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_porfile);
+
+        Button btnSave = findViewById(R.id.button3);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/androidpprog2/regals/Profile.java
+++ app/src/main/java/com/androidpprog2/regals/Profile.java
@@ -2,7 +2,10 @@ package com.androidpprog2.regals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.porfile);
+        Button btnProfile = findViewById(R.id.button6);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditProfile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
